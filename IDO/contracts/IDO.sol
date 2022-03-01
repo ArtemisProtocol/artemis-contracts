cdb2b9ca3e1782c37ddface62475c896287fe2ee
Luke Hannigan <lukeyhannigany@gmail.com>
2022-03-01 20:51:33 +0000

    IDO updated

--- IDO/contracts/IDO.sol
+++ IDO/contracts/IDO.sol
@@ -1,214 +1,194 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "./IIDO.sol";
 
-struct Participant {
-    bool collateralPaid;
-    bool collateralRefunded;
+contract IDO is IIDO, Ownable {
 
-    uint bought;
-    uint claimed;
-}
+    IERC20 _token;
+    uint _forSale;
 
-contract IDO is IIDO, Ownable, Pausable {
+    uint _asking;
+    uint _contributed;
 
-    //Parameters for ICO.
-    Params _params;
+    uint _buyingStartsAt;
+    uint _buyingEndsAt;
 
-    //How many tokens have sold.
-    uint _sold;
-    //How many tokens have been claimed.
-    uint _claimed;
+    uint _vestingStartsAt;
+    uint _vestingEndsAt;
 
-    //Have paramaters been set yet?
-    bool _initialised;
+    uint _unsoldWithdrawn;
+    uint _ONEWithdrawn;
 
-    //Data of participants.
-    mapping(address => Participant) _participants;
+    mapping(address => UserStats) _userStats;
 
-    function getParams() public override view returns(Params memory) {
-        return _params;
+    constructor(IERC20 token, uint forSale, uint asking, uint buyingStartsAt, uint buyingEndsAt, uint vestingStartsAt, uint vestingEndsAt) {
+        token.transferFrom(msg.sender, address(this), forSale);
+        _asking = asking;
+        _forSale = forSale;
+        _buyingStartsAt = buyingStartsAt;
+        _buyingEndsAt = buyingEndsAt;
+        _vestingStartsAt = vestingStartsAt;
+        _vestingEndsAt = vestingEndsAt;
     }
 
-    function initialise(Params memory params) public override onlyOwner {
-
-        require(!_initialised, "ICO already initialised.");
-
-        require(block.timestamp >= params.start, "ICO cannot begin before it is initialised.");
-        require(params.end >= params.start, "ICO cannot end before it begins.");
-        require(params.vestingStart >= params.end, "Vesting cannot start before ICO ends.");
-        require(params.vestingEnd >= params.vestingStart, "Vesting cannot end before it begins.");
-
-        require(params.withdrawee != address(0), "Withdrawee cannot be zero address.");
-
-        _params = params;
-        _initialised = true;
-
-        emit Initialised();
-
+    function getToken() external override view returns(IERC20) {
+        return _token;
     }
-
-    /*
-        Halts these actions:
-        - Depositing collateral
-        - Purchasing tokens
-        - Claiming tokens
-
-        Users will still be able to retrieve their collateral.
-        Once the ICO is underway, and the values are all correct, owner may decide to renounce ownership so that investors won't fear this function.
-    */
-    function pause() public override onlyOwner {
-        _pause();
+    function getForSale() external override view returns(uint) {
+        return _forSale;
     }
-
-    /*
-        Resumes these actions:
-        - Depositing collateral
-        - Purchasing tokens
-        - Claiming tokens
-    */
-    function unpause() public override onlyOwner {
-        _unpause();
+    function getAsking() external override view returns(uint) {
+        return _asking;
     }
-
-    function status() public override view returns(Status) {
-
-        require(_initialised, "ICO has not been initialised.");
-
-        if(block.timestamp < _params.start) {
-            return Status.awaitingstart;
+    function getContributed() external override view returns(uint) {
+        return _contributed;
+    }
+    function getBuyingStartsAt() external override view returns(uint) {
+        return _buyingStartsAt;
+    }
+    function getBuyingEndsAt() external override view returns(uint) {
+        return _buyingEndsAt;
+    }
+    function getVestingStartsAt() external override view returns(uint) {
+        return _vestingStartsAt;
+    }
+    function getVestingEndsAt() external override view returns(uint) {
+        return _vestingEndsAt;
+    }
+    function getTotalOwedOf(address addr) external override view returns(uint) {
+        return _getTotalOwedOfStats(_userStats[addr]);
+    }
+    function getUnsoldTokens() public override view returns(uint unsold) {
+        if(_contributed >= _asking) {
+            return 0;
         }
+        unsold = _forSale - ((_contributed * _asking) / _forSale);
+    }
+    function getUnsoldTokensWithdrawn() external override view returns(uint) {
+        return _unsoldWithdrawn;
+    }
 
-        if(block.timestamp <= _params.end) {
-            return Status.inprogress;
+    //purchase tokens.
+    function contribute() external override payable {
+        //must be during buying period.
+        require(block.timestamp >= _buyingStartsAt);
+        require(block.timestamp < _buyingEndsAt);
+
+        //update user data based on transaction value.
+        _userStats[msg.sender].contributed += msg.value;
+    }
+
+    //claim purchased tokens.
+    function claim() external override {
+        UserStats storage userStats = _userStats[msg.sender];
+        //get the claimable amount.
+        uint toClaim = _getClaimableOfStats(userStats);
+        //transfer the claimable amount.
+        _token.transfer(msg.sender, toClaim);
+        //update user data.
+        userStats.claimed += toClaim;
+    }
+
+    //get the data of a certain user.
+    function getUserStatsOf(address addr) external override view returns(UserStats memory) {
+        return _userStats[addr];
+    }
+
+    //get the amount claimable for a user.
+    function getClaimableOf(address addr) external override view returns(uint) {
+        return _getClaimableOfStats(_userStats[addr]);
+    }
+
+    //refund excess ONE in the case of overflow.
+    function refund() external override {
+        require(block.timestamp >= _buyingEndsAt);
+
+        UserStats storage userStats = _userStats[msg.sender];
+
+        uint toRefund = _getRefundableOfStats(userStats);
+        _token.transfer(msg.sender, toRefund);
+
+        userStats.refunded += toRefund;
+    }
+
+    //withdraw tokens which did not sell.
+    function withdrawUnsoldTokens(address to) external override onlyOwner {
+        require(block.timestamp >= _buyingEndsAt);
+
+        uint unsold = getUnsoldTokens();
+        uint toWithdraw = unsold - _unsoldWithdrawn;
+
+        _token.transfer(to, toWithdraw);
+        _unsoldWithdrawn += toWithdraw;
+    }
+
+    //withdraw ONE.
+    function withdrawONE() external override onlyOwner {
+        require(block.timestamp >= _buyingEndsAt);
+        uint totalWithdrawable;
+        if(_contributed <= _asking) {
+            totalWithdrawable = _contributed;
+        } else {
+            totalWithdrawable = _asking;
         }
-
-        if(block.timestamp < _params.vestingStart) {
-            return Status.awaitingvesting;
-        }
-
-        if(block.timestamp <= _params.vestingEnd) {
-            return Status.vesting;
-        }
-
-        return Status.vestingended;
-
-    }
-
-    function depositCollateral() public override whenNotPaused {
-
-        require(status() == Status.awaitingstart, "ICO has already begun.");
-
-        Participant storage participant = _participants[msg.sender];
-
-        require(!participant.collateralPaid);
-
-        _params.collateralToken.transferFrom(msg.sender, address(this), _params.collateralAmount);
-        participant.collateralPaid = true;
-
-        emit CollateralPaid(msg.sender);
-
-    }
-
-    receive() external override whenNotPaused payable {
-
-        require(status() == Status.inprogress, "ICO is not in progress.");
-
-        uint toPurchase = (msg.value * _params.price.inReturn) / _params.price.asking;
-        require(toPurchase <= _params.forSale - _sold, "Sold out.");
-        _sold += toPurchase;
-
-        Participant storage participant = _participants[msg.sender];
-
-        require(participant.collateralPaid, "Collateral not paid.");
-
-        participant.bought += toPurchase;
-
-        emit TokensPurchased(msg.sender, toPurchase);
-
-    }
-
-    function retrieveCollateral() public override {
-
-        require(_initialised, "ICO has not been initialised.");
-        require(block.timestamp >= _params.end, "ICO has not ended yet.");
-
-        Participant storage participant = _participants[msg.sender];
-
-        require(participant.collateralPaid && !participant.collateralRefunded, "No collateral to retrieve.");
-
-        _params.collateralToken.transfer(msg.sender, _params.collateralAmount);
-        participant.collateralRefunded = true;
-
-        emit CollateralRefunded(msg.sender);
-
-    }
-
-    function pendingClaimOf(address addr) public override view returns(uint) {
-
-        require(_initialised, "ICO not initialised.");
-        require(block.timestamp >= _params.vestingStart, "Vesting not started.");
-
-        Participant storage participant = _participants[addr];
-
-        //Time since vesting began.
-        uint timeSince = block.timestamp - _params.vestingStart;
-        //Overall time that vesting lasts.
-        uint maxTime =_params. vestingEnd - _params.vestingStart;
-        //Vested time is the greater value of the two.
-        uint vestedTime = timeSince <= maxTime ? timeSince : maxTime;
-        //Claimable up until this point in time.
-        uint maxClaimable = (participant.bought * vestedTime) / maxTime;
-        //Subtract total claimed already.
-        uint claimable = maxClaimable - participant.claimed;
-
-        return claimable;
-
-    } 
-
-    function claim() public override whenNotPaused {
-
-        require(_initialised, "ICO not initialised.");
-        require(block.timestamp >= _params.vestingStart, "Vesting not started.");
-
-        uint toClaim = pendingClaimOf(msg.sender);
-
-        Participant storage participant = _participants[msg.sender];
-
-        _params.token.transfer(msg.sender, toClaim);
-        participant.claimed += toClaim;
-        _claimed += toClaim;
-
-        emit TokensClaimed(msg.sender, toClaim);
-
-    }
-
-    //Withdraw all ONE and unsold tokens to withdrawee. Unrestricted, so that nobody can withhold the transfer to the rightful receiver.
-    function finalWithdraw() public override {
-
-        require(_initialised, "ICO has not been initialised.");
-
-        require(block.timestamp >= _params.end, "ICO not ended.");
-
-        (bool success,) = _params.withdrawee.call{value: address(this).balance}("");
+        uint withdrawable = totalWithdrawable - _ONEWithdrawn;
+        _ONEWithdrawn -= withdrawable;
+        (bool success,) = msg.sender.call{value: withdrawable}("");
         require(success);
-
-        _params.token.transfer(_params.withdrawee, _sold - _claimed);
-
-        emit FinalWithdraw();
-
     }
 
-    //Forcefully withdraw all balances. However, once the ICO is underway and the values are all correct, owner may decide to renounce ownership so that investors won't fear this function.
-    function forceWithdraw(address to) public override {
+    //forcefully withdraw tokens.
+    function forceWithdrawTokens(address addr, uint amount) external override onlyOwner {
+        _token.transfer(addr, amount);
+    }
 
-        (bool success,) = to.call{value: address(this).balance}("");
+    //forcefully withdraw ONE.
+    function forceWithdrawONE(address addr, uint amount) external override onlyOwner {
+        (bool success,) = addr.call{value: amount}("");
         require(success);
-
-        _params.token.transfer(to, _params.token.balanceOf((address(this))));
-
     }
+
+    function _getClaimableOfStats(UserStats storage userStats) private view returns(uint claimable) {
+        //vesting hasnt begun yet.
+        if(block.timestamp < _vestingStartsAt) {
+            return 0;
+        }
+        //maximum possible time vested.
+        uint maximumTimeVested = _vestingEndsAt - _vestingStartsAt;
+        //time since vesting started.
+        uint timeSinceVestingStarted = block.timestamp - _vestingStartsAt;
+        //time vested, taking into account maximum time vested.
+        uint timeVested = timeSinceVestingStarted <= maximumTimeVested ? timeSinceVestingStarted : maximumTimeVested;
+        //the amount owed over the entire vesting period.
+        uint totalOwed = _getTotalOwedOfStats(userStats);
+        //the amount owed if none has been claimed yet.
+        uint owedSinceVestingStarted = (totalOwed * timeVested) / maximumTimeVested;
+        //the amount after the claimed amount is taken into account.
+        claimable = owedSinceVestingStarted - userStats.claimed;
+    }
+
+    function _getTotalOwedOfStats(UserStats storage userStats) private view returns(uint totalOwed) {
+        if(_contributed > _asking) {
+            //when there is overflow we use the amount contributed overall.
+            totalOwed = (userStats.contributed * _forSale) / _contributed;
+        } else {
+            //when there isn't overflow we use the original price.
+            totalOwed = (userStats.contributed * _asking) / _forSale; 
+        }
+    }
+
+    function _getRefundableOfStats(UserStats storage userStats) private view returns(uint refundable) {
+        //if there is no overflow there is no ONE to refund. 
+        if(_contributed <= _asking) {
+            return 0;
+        }
+        //the amount refundable if none has been refunded yet.
+        uint totalRefundable = (userStats.contributed * _contributed) / _asking;
+        //the amount refundable after the refunded amount is taken into account.
+        refundable = totalRefundable - userStats.refunded;
+    }
+
+
 
 }
--- IDO/contracts/IIDO.sol
+++ IDO/contracts/IIDO.sol
@@ -1,82 +1,30 @@
 //SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-enum Status {
-
-    awaitingstart,
-    inprogress,
-    awaitingvesting,
-    vesting,
-    vestingended
-
+struct UserStats {
+    uint contributed;
+    uint refunded;
+    uint claimed;
 }
-
-//Ratio for price. An example would be 10 wei for 2 wei of token, or 5000 wei for 350 wei of token.
-struct Price {
-    uint asking;
-    uint inReturn;
-}
-
-struct Params {
-
-    IERC20 token;
-    IERC20 collateralToken;
-
-    //When tokens start being purchaseable.
-    uint start;
-    //When tokens stop being purchaseable.
-    uint end;
-
-    //When tokens start being linearly released.
-    uint vestingStart;
-    //When tokens stop being linearly released.
-    uint vestingEnd;
-
-    //How many tokens are for sale.
-    uint forSale;
-
-    //Price ratio from ONE to tokens.
-    Price price;
-
-    //Amount of collateral to lock in during ICO.
-    uint collateralAmount;
-
-    //Receiver of final withdraw
-    address withdrawee;
-
-}
-
 interface IIDO {
-
-    event Initialised();
-    event TokensPurchased(address indexed addr, uint amount);
-    event TokensClaimed(address indexed addr, uint amount);
-    event FinalWithdraw();
-    event CollateralPaid(address indexed addr);
-    event CollateralRefunded(address indexed addr);
-
-    function getParams() external view returns(Params memory);
-
-    function initialise(Params memory params) external;
-
-    function pause() external;
-
-    function unpause() external;
-
-    function status() external view returns(Status);
-
-    function depositCollateral() external;
-
-    receive() external payable;
-
-    function retrieveCollateral() external;
-
-    function pendingClaimOf(address addr) external view returns(uint);
-
+    function getToken() external view returns(IERC20);
+    function getForSale() external view returns(uint);
+    function getAsking() external view returns(uint);
+    function getContributed() external view returns(uint);
+    function getBuyingStartsAt() external view returns(uint);
+    function getBuyingEndsAt() external view returns(uint);
+    function getVestingStartsAt() external view returns(uint);
+    function getVestingEndsAt() external view returns(uint);
+    function getUnsoldTokens() external view returns(uint unsold);
+    function getUnsoldTokensWithdrawn() external view returns(uint);
+    function contribute() external payable;
     function claim() external;
-
-    function finalWithdraw() external;
-
-    function forceWithdraw(address to) external;
-
+    function refund() external;
+    function getUserStatsOf(address addr) external view returns(UserStats memory);
+    function getTotalOwedOf(address addr) external view returns(uint);
+    function getClaimableOf(address addr) external view returns(uint);
+    function withdrawUnsoldTokens(address to) external;
+    function withdrawONE() external;
+    function forceWithdrawTokens(address addr, uint amount) external;
+    function forceWithdrawONE(address addr, uint amount) external;
 }
