7138e20aba117f5e6099b9b21e27f8d29c4badac
Luke Hannigan <lukeyhannigany@gmail.com>
2022-03-04 18:18:06 +0000

    added compileandflatten task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
+
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.12",
 };
+task("compileandflatten", async () => {
+  await hre.run("compile")
+  await hre.run("flatten")
+})
