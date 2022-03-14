2ad9b172522c61dd2642ea9b1da2f41d331536fc
Luke Hannigan <lukeyhannigany@gmail.com>
2022-03-14 17:30:14 +0000

    mistake

--- contracts/extensions/IDOWithCollateral/IDOWithCollateral.sol
+++ contracts/extensions/IDOWithCollateral/IDOWithCollateral.sol
@@ -14,7 +14,7 @@ abstract contract IDOWithCollateral is IIDOWithCollateral, IDO, AccessControlEnu
         return _collateralInfo;
     }
     function collateralise() external override {
-        require(block.timestamp < _parameters.buyingEndsAt, "Cannot collateralise after buying ends.");
+        require(block.timestamp < _parameters.buyingStartsAt, "Cannot collateralise after buying starts.");
         require(!hasRole(COLLATERALISED_ROLE, msg.sender), "Already collateralised.");
         _collateralInfo.token.transferFrom(msg.sender, address(this), _collateralInfo.amount);
         _grantRole(COLLATERALISED_ROLE, msg.sender);
