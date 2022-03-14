1dbb4224694e3bd420dbcc4ae567c169c604eeec
Luke Hannigan <lukeyhannigany@gmail.com>
2022-03-14 22:20:14 +0000

    Using safeERC20 for token transfers, and using reentrancy guard in collateralising, claiming, and refunding

--- contracts/IDOWithJustWhitelist.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicensed
-import "./extensions/IDOWithWhitelist/IDOWithWhitelist.sol";
-pragma solidity ^0.8.0;
-contract IDOWithJustWhitelist is IDOWithWhitelist {
-    constructor(Parameters memory parameters) IDO(parameters) {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    }
-}
